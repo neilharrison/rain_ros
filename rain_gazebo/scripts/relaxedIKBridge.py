53a780b564770123f2efa079d8be9196921e67b1
Neil Harrison <ntharrison12@gmail.com>
2020-03-24 14:13:53 +0000

    Added relaxedIK Bridge to connect relaxedIK to Gazebo -New ur5 launch file

---
+++ rain_gazebo/scripts/relaxedIKBridge.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+from std_msgs.msg import String
+from std_msgs.msg import Float64MultiArray
+from relaxed_ik.msg import JointAngles
+from sensor_msgs.msg  import JointState
+
+def listener():
+
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # name are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+    rospy.init_node('relaxedIKGazebo', anonymous=True)
+
+    rospy.Subscriber("relaxed_ik/joint_angle_solutions", JointAngles, talker)
+    
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+
+def talker(jAngles):
+    pub = rospy.Publisher('joint_group_position_controller/command', Float64MultiArray, queue_size=10)
+
+    jointAngles = Float64MultiArray()
+    #print(jAngles.angles.data)
+    
+    jointAngles.data = jAngles.angles.data
+    
+    jointAngles.data = [jointAngles.data[0], jointAngles.data[1]-1.57,jointAngles.data[2],jointAngles.data[3]-1.57,jointAngles.data[4],jointAngles.data[5]]
+    #rospy.loginfo(jointAngles)
+    pub.publish(jointAngles)
+    
+
+if __name__ == '__main__':
+    listener()
